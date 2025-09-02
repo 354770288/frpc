835bd481f345ff0551cf228642402499a4da1749
zhangxing <36475083+354770288@users.noreply.github.com>
2025-09-02 21:43:16 +0800

    Update frpc_linux_install.sh

--- frpc_linux_install.sh
+++ frpc_linux_install.sh
@@ -49,7 +49,7 @@ while ! test -z "$(ps -A | grep -w ${FRP_NAME})"; do
 done
 
 mkdir -p ${FRP_PATH}
-wget -P ${WORK_PATH} https://ghproxy.com/https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/${FILE_NAME}.tar.gz -O ${FILE_NAME}.tar.gz && \
+wget -P ${WORK_PATH} https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/${FILE_NAME}.tar.gz -O ${FILE_NAME}.tar.gz && \
 tar -zxvf ${FILE_NAME}.tar.gz && \
 mv ${FILE_NAME}/${FRP_NAME} ${FRP_PATH}
 
